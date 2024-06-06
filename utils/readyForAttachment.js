e5e29cebcb37a2f9869f5095ecca2b729c6cc70c
Josh Rosenfeld <joshr@rogers.com>
2024-06-06 12:19:56 -0400

    update requirements for PDF to generate

--- utils/readyForAttachment.js
+++ utils/readyForAttachment.js
@@ -3,12 +3,19 @@ const smartsheet = require("../modules/smartsheet");
 const readyForAttachment = async (sheetId, rowId) => {
   // Check In-Queue status
   const inQueueArrayIndex = 12; // TODO: change when migrating sheets
+  const orderIdArrayIndex = 0;
+
   const row = await smartsheet.getRow(sheetId, rowId);
   if (row.cells[inQueueArrayIndex].value) return false;
 
-  // Check if current row already has an attachment
-  const rowAttachments = await smartsheet.getRowAttachments(sheetId, rowId);
-  return rowAttachments.data.length === 0 ? true : false;
+  // Check if PDF for given orderId already exists
+  const orderId = row.cells[orderIdArrayIndex].value;
+  const { data: attachments } = await smartsheet.getAttachments(sheetId);
+  for (const attachment of attachments) {
+    if (attachment.name === `${orderId}.pdf`) return false;
+  }
+
+  return true;
 };
 
 module.exports = readyForAttachment;
